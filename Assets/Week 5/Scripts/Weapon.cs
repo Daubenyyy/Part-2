7291d77f51b89d8b664ced1e579056d730a9e3ad
Jack Mitchell <mitcjack@shernet.sheridancollege.ca>
2024-02-08 12:09:33 -0500

    task 4a completed: added weapon

---
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float speed = 3f;
+    Rigidbody2D rb;
+    float counter;
+    public float maxCount = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if(counter == maxCount)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            counter ++;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SendMessage("TakeDamage", 1);
+    }
+}
